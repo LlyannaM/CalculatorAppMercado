454c66ccaae0ed2c4c99d45054e85ec364965d55
LlyannaM <113721009+LlyannaM@users.noreply.github.com>
2023-09-09 20:44:56 -0500

    worked on calculation method

--- app/src/main/java/com/example/calculatorappmercado/CalculatorActivity.java
+++ app/src/main/java/com/example/calculatorappmercado/CalculatorActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -12,13 +16,35 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        Button button = (R.id.)
+        createEquation()
+        double answer;
+
+        TextView equationTV = findViewById(R.id.equationView);
+        TextView answerTV = findViewById(R.id.answerView);
+        
+    }
+
+    public double createEquation (View view, Button calcButton) {
+        String equation = "";
+        if (calcButton == findViewById(R.id.sevenButton)) {
+            equation += 7;
+        }
+        else if (calcButton == findViewById(R.id.eightButton)) {
+            equation += 8;
+        }
+
+        double answer = 0;
+       return answer;
+
+
+
     }
 
 
-    public void createEquation (View v) {
 
 
-    }
+
 
 
 
