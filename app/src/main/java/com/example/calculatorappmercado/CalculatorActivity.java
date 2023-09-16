7871ae9070aaf5eed406920c14418ad3473b13ed
LlyannaM <113721009+LlyannaM@users.noreply.github.com>
2023-09-16 17:35:59 -0500

    cited resources w/ comments

--- app/src/main/java/com/example/calculatorappmercado/CalculatorActivity.java
+++ app/src/main/java/com/example/calculatorappmercado/CalculatorActivity.java
@@ -21,83 +21,89 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
     }
 
-    public void createEquation(View numButton) {
-        if (numButton.getId() == R.id.multiplyButton) {
+
+    public void createEquation(View v) {
+        if (v.getId() == R.id.multiplyButton) {
             Log.i("Mercado", "Selected multiplication symbol");
             equation += "*";
-        } else if (numButton.getId() == R.id.sevenButton) {
+        } else if (v.getId() == R.id.sevenButton) {
             Log.i("Mercado", "Selected 7");
             equation += "7";
-        } else if (numButton.getId() == R.id.eightButton) {
+        } else if (v.getId() == R.id.eightButton) {
             Log.i("Mercado", "Selected 8");
             equation += "8";
-        } else if (numButton.getId() == R.id.nineButton) {
+        } else if (v.getId() == R.id.nineButton) {
             Log.i("Mercado", "Selected 9");
             equation += "9";
-        } else if (numButton.getId() == R.id.fourButton) {
+        } else if (v.getId() == R.id.fourButton) {
             Log.i("Mercado", "Selected 4");
             equation += "4";
-        } else if (numButton.getId() == R.id.fiveButton) {
+        } else if (v.getId() == R.id.fiveButton) {
             Log.i("Mercado", "Selected 5");
             equation += "5";
-        } else if (numButton.getId() == R.id.sixButton) {
+        } else if (v.getId() == R.id.sixButton) {
             Log.i("Mercado", "Selected 6");
             equation += "6";
-        } else if (numButton.getId() == R.id.minusButton) {
+        } else if (v.getId() == R.id.minusButton) {
             Log.i("Mercado", "Selected -");
             equation += "-";
-        } else if (numButton.getId() == R.id.divideButton) {
+        } else if (v.getId() == R.id.divideButton) {
             Log.i("Mercado", "Selected divide symbol");
             equation += "/";
-        } else if (numButton.getId() == R.id.oneButton) {
+        } else if (v.getId() == R.id.oneButton) {
             Log.i("Mercado", "Selected 1");
             equation += "1";
-        } else if (numButton.getId() == R.id.twoButton) {
+        } else if (v.getId() == R.id.twoButton) {
             Log.i("Mercado", "Selected 2");
             equation += "2";
-        } else if (numButton.getId() == R.id.threeButton) {
+        } else if (v.getId() == R.id.threeButton) {
             Log.i("Mercado", "Selected 3");
             equation += "3";
-        } else if (numButton.getId() == R.id.plusButton) {
+        } else if (v.getId() == R.id.plusButton) {
             Log.i("Mercado", "Selected +");
             equation += "+";
-        } else if (numButton.getId() == R.id.zeroButton) {
+        } else if (v.getId() == R.id.zeroButton) {
             Log.i("Mercado", "Selected 0");
             equation += "0";
         }
 
         TextView equationTV = findViewById(R.id.equationView);
         equationTV.setText(equation);
-
     }
 
-
-    public void clearEquation(View clearButton) {
-        Log.i("Mercado", "Deleted character in the equation");
-        if (equation.length() != 0) {
-            equation = equation.substring(0, equation.length() - 1);
-            TextView equationTV = findViewById(R.id.equationView);
-            equationTV.setText(equation);
+    public void clearEquation(View v) {
+        TextView equationTV = findViewById(R.id.equationView);
+        if(v.getId() == R.id.clearButton) {
+            if (equation.length() != 0) {
+                equation = equation.substring(0, equation.length() - 1);
+                Log.i("Mercado", "Deleted a character in the equation");
+                equationTV.setText(equation);
+            }
+        }
+        else {
+            equation = "";
+            equationTV.setText("");
+            Log.i("Mercado", "Cleared equation");
         }
     }
 
 
     public void findNumList () {
         numList.clear();
-        Log.i("Mercado", "findNumListcalled");
         int startIndex = 0;
         int endIndex = 0;
-
         while (endIndex <= equation.length()) {
+            // I used the isDigit() function to check if a character in a certain index of the equation
+            // was a number. I learned how to do this here: https://www.scaler.com/topics/isdigit-java/.
             if (endIndex == equation.length() || !Character.isDigit(equation.charAt(endIndex))) {
                 if (endIndex > startIndex) {
                     String numberStr = equation.substring(startIndex, endIndex);
-                    double number = 0;
-                    number = Double.parseDouble(numberStr);
+                    double number = Double.parseDouble(numberStr);
                     numList.add(number);
-                    Log.i("Mercado", "findNumList currently adding " + numberStr);
+                    Log.i("Mercado", "currently adding " + numberStr + " to numList");
                 }
                 startIndex = endIndex + 1;
             }
@@ -105,16 +111,14 @@ public class CalculatorActivity extends AppCompatActivity {
         }
         if (equation.charAt(0) == '-')
             numList.set(0, -numList.get(0));
-
-        Log.i("Mercado", "findNumList CREATED" + numList.toString());
+        Log.i("Mercado", "NumList created: " + numList.toString());
             }
 
     public void setOperationList () {
         operationList.clear();
-        Log.i("Mercado", "operation list making" + equation);
+
         equation = equation.replace("+-", "-");
         equation = equation.replace("--", "+");
-        Log.i("Mercado", "operation list edited" + equation);
 
         for (int i = 0; i < equation.length(); i++) {
             if (equation.charAt(i) == '+' || (equation.charAt(i) == '-' && i != 0)
@@ -122,25 +126,22 @@ public class CalculatorActivity extends AppCompatActivity {
                 operationList.add(String.valueOf(equation.charAt(i)));
             }
         }
-        Log.i("Mercado", "operation list created" + operationList.toString());
-        // if num elements in operationList is greater than numList, set error
+        Log.i("Mercado", "operation list created: " + operationList.toString());
 
     }
 
     public void showResult(View v) {
-        Log.i("Mercado", "calculating result");
+        Log.i("Mercado", "Calculating result");
         TextView resultTV = findViewById(R.id.answerView);
         double result = 0;
         if(equation.length() != 0) {
             findNumList();
             setOperationList();
             if (operationList.size() >= numList.size()) {
-                resultTV.setText("Error");
-            } else {
-
-
+                resultTV.setText("Syntax Error");
+            }
+            else {
                 while (numList.size() > 1) {
-
                     if (operationList.indexOf("*") != -1 || operationList.indexOf("/") != -1) {
                         int multiplicationIndex = operationList.indexOf("*");
                         int divisionIndex = operationList.indexOf("/");
@@ -174,14 +175,14 @@ public class CalculatorActivity extends AppCompatActivity {
                 resultTV.setText("= " + result);
             }
         }
+        else{
+            resultTV.setText("= ");
+        }
     }
 
 
-
     public void performOperation(int indexOfOperation, String operation) {
-        Log.i("Mercado", "performOperationCalled" + operation + indexOfOperation);
-        Log.i("Mercado", numList.toString());
-        Log.i("Mercado", operationList.toString());
+
         double result = 0;
 
         Log.i("Mercado", String.valueOf(indexOfOperation));
@@ -199,10 +200,11 @@ public class CalculatorActivity extends AppCompatActivity {
         else if (operation.equals("/"))
             result = firstNum / secondNum;
 
-
         numList.set(indexOfOperation, result);
         numList.remove(indexOfOperation+1);
         operationList.remove(indexOfOperation);
+
+        Log.i("Mercado", "Operation performed: " + operation);
         Log.i("Mercado", numList.toString());
         Log.i("Mercado", operationList.toString());
 
